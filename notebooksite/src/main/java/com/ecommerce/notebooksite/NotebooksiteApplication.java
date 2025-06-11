2c04e3394b6b309bed1c21f05cd4adef782a3da1
suyash <shevadesuyash@gmail.com>
2025-06-11 16:34:07 +0530

    Initial Commit base project till liquibase setup

---
+++ notebooksite/src/main/java/com/ecommerce/notebooksite/NotebooksiteApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.notebooksite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebooksiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebooksiteApplication.class, args);
+	}
+
+}
---
+++ notebooksite/src/test/java/com/ecommerce/notebooksite/NotebooksiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.notebooksite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebooksiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
